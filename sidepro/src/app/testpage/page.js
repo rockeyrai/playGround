2e97dfdf053d3401199bb3b1d132f9df015201d2
Rockey C.R <rockeyrai234@gmail.com>
2024-09-20 11:38:39 +0545

    setting put the project with NextUI and react icon

---
+++ sidepro/src/app/testpage/page.js
@@ -0,0 +1,15 @@
+import { Button } from '@nextui-org/react'
+import { AiFillAccountBook } from "react-icons/ai";
+import React from 'react'
+
+const testpage = () => {
+  return (
+    <div>
+      <Button>hello</Button>
+      <AiFillAccountBook />
+      
+    </div>
+  )
+}
+
+export default testpage
